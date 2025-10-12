68cdfe57f725ee460586df922577b3d480af7732
StevenSYS <139715581+StevenSYS@users.noreply.github.com>
2025-10-12 18:34:08 +0000

    Added a GitHub workflow and dummy FMOD header files

---
+++ src/dummyFMOD/fmod.h
@@ -0,0 +1,35 @@
+#ifndef _FMOD_H
+#define _FMOD_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "fmod_common.h"
+
+#define FMOD_Channel_Set3DAttributes(...) FMOD_OK
+#define FMOD_Sound_Set3DMinMaxDistance(...) FMOD_OK
+#define FMOD_System_Create(...) FMOD_OK
+#define FMOD_System_CreateSound(...) FMOD_OK
+#define FMOD_Channel_SetVolumeRamp(...) FMOD_OK
+#define FMOD_System_Close(...) FMOD_OK
+#define FMOD_Sound_Release(...) FMOD_OK
+#define FMOD_System_Update(...) FMOD_OK
+#define FMOD_Channel_SetLowPassGain(...) FMOD_OK
+#define FMOD_Channel_SetPan(...) FMOD_OK
+#define FMOD_Channel_SetVolume(...) FMOD_OK
+#define FMOD_Channel_SetPaused(...) FMOD_OK
+#define FMOD_Channel_Stop(...) FMOD_OK
+#define FMOD_Channel_SetMode(...) FMOD_OK
+#define FMOD_System_PlaySound(...) FMOD_OK
+#define FMOD_ChannelGroup_SetVolume(...) FMOD_OK
+#define FMOD_System_Set3DListenerAttributes(...) FMOD_OK
+#define FMOD_System_Init(...) FMOD_OK
+#define FMOD_System_GetMasterChannelGroup(...) FMOD_OK
+#define FMOD_System_Release(...) FMOD_OK
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ src/dummyFMOD/fmod_errors.h
@@ -0,0 +1,10 @@
+#ifndef _FMOD_ERRORS_H
+#define _FMOD_ERRORS_H
+
+#include "fmod.h"
+
+static const char *FMOD_ErrorString(FMOD_RESULT errcode) {
+	return "Dummy FMOD";
+}
+
+#endif
